1aae5938479f6c33a6b2725b54100c2eaa9ea777
Stephen Hodgson <hodgson.designs@gmail.com>
2021-06-24 08:27:23 -0400

    updated project
    added basic assembly info

---
+++ XRTK.XRUX/Packages/com.xrtk.xrux/Runtime/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#define XRUX
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyTitle("com.xrtk.xrux")]
+[assembly: AssemblyCompany("XRTK")]
+[assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
+
+// Note: these are the names of the assembly definitions themselves, not necessarily the actual namespace the class is in.
+[assembly: InternalsVisibleTo("XRUX.Editor")]
+[assembly: InternalsVisibleTo("XRUX.Tests")]
